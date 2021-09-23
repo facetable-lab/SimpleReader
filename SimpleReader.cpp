999e71be0ae04f123386c05a1eae4d84720fa730
facetable-lab <67112192+facetable-lab@users.noreply.github.com>
2021-09-23 17:06:07 +0300

    Add files via upload

---
+++ SimpleReader.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Memory.h"
+
+using namespace std;
+
+memory mem;
+PModule bClient;
+
+int main() {
+	while(!mem.Attach("csgo.exe", PROCESS_ALL_ACCESS)) {}
+	bClient = mem.GetModule("client.dll");
+	
+	cout << "dwBase: " << bClient.dwBase << endl;
+	cout << "dwSize: " << bClient.dwSize << endl;
+
+	cout << "some value" << mem.Read<DWORD>(bClient.dwBase) << endl;
+
+	mem.Exit();
+	system("pause");
+
+	return 0;
+}
+
